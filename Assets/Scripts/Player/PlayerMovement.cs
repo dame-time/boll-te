88210d73ad808d59c7f575cd31680320bdc70ce4
ilSubex <jesussubero96@gmail.com>
2025-01-25 14:37:07 +0100

    mixing playermovement with new system pickable

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -20,6 +20,8 @@ public class PlayerMovement : MonoBehaviour
     private InputAction playerControls;
     [SerializeField]
     private InputAction interaction;
+    [SerializeField]
+    private InputAction drop;
 
     [SerializeField]
     private Animator playerAnimator;
@@ -33,18 +35,27 @@ public class PlayerMovement : MonoBehaviour
     private bool canMove;
     private PlayerBackpack _playerBackpack;
     private Stations _stations;
-    
+
+    [SerializeField]
+    private GameObject hardcodePosition;
+    [SerializeField]
+    private GameObject objectTest;
+
+    private GameObject tempHold;
+
     private void Awake()
     {
         _playerBackpack = GetComponent<PlayerBackpack>();
         _stations = FindObjectOfType<Stations>();
     }
-    
+
     private void OnEnable()
     {
         playerControls.Enable();
         interaction.Enable();
+        drop.Enable();
         interaction.performed += Interact;
+        drop.performed += Drop;
 
     }
 
@@ -54,6 +65,7 @@ public class PlayerMovement : MonoBehaviour
         playerControls.Disable();
         interaction.Disable();
         interaction.performed -= Interact;
+        drop.performed -= Drop;
     }
 
     // Update is called once per frame
@@ -86,6 +98,12 @@ public class PlayerMovement : MonoBehaviour
             rigibody.velocity = Vector3.zero;
         }
 
+        if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("AN_Gigino_GrabLoop") && tempHold)
+        {
+            tempHold.transform.position = hardcodePosition.transform.position;
+            tempHold.transform.SetParent(hardcodePosition.transform);
+        }
+
 
 
     }
@@ -93,19 +111,43 @@ public class PlayerMovement : MonoBehaviour
     private void Interact(InputAction.CallbackContext context)
     {
         if (_playerBackpack.isHoldingObject || !canGrab) return;
-        
+
+
+
         //print("Interaction performed");
         playerAnimator.SetBool("grab", true);
         playerAnimator.SetFloat("velocity", 0);
         // objectTest.transform.position = hardcodePosition.transform.position;
         // objectTest.transform.SetParent(hardcodePosition.transform);
-        StartCoroutine(ExampleCoroutine());
+        StartCoroutine(ExampleCoroutine("grab"));
     }
 
-    IEnumerator ExampleCoroutine()
+    //private void Drop(InputAction.CallbackContext context)
+    //{
+    //    //print("Interaction performed");
+    //    if (tempHold)
+    //    {
+    //        playerAnimator.SetBool("drop", true);
+    //        tempHold.transform.position = hardcodePosition.transform.position;
+    //        tempHold.GetComponent<Rigidbody>().useGravity = true;
+    //        tempHold.transform.SetParent(null);
+    //        StartCoroutine(ExampleCoroutine("drop"));
+    //        tempHold = null;
+    //    }
+
+    //}
+
+    IEnumerator ExampleCoroutine(string reset)
     {
-        yield return new WaitForSeconds(0.2f);
-        playerAnimator.SetBool("grab", false);
         _stations.GetActiveStation().GrabItem();
+        print("is holding backpack: ?" + _playerBackpack.isHoldingObject);
+        print("what object?: " + _playerBackpack.objectHolded);
+        tempHold = Instantiate(_playerBackpack.objectHolded, hardcodePosition.transform.position, hardcodePosition.transform.rotation);
+        tempHold.transform.localScale = new Vector3(_playerBackpack.transform.localScale.x, _playerBackpack.transform.localScale.y, _playerBackpack.transform.localScale.z);
+        tempHold.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        playerAnimator.SetBool(reset, false);
+
+
     }
 }
